1d94346ac6fa85e424f19b9d83dab10a36453ffa
F-Neto15 <chicaoneto15@gmail.com>
2026-09-13 19:37:04 -0300

    neto questão 9.8

--- neto_q9.8.c
+++ neto_q9.8.c
@@ -9,25 +9,25 @@ typedef struct No {
 
 char enesimo(int n, No *L) {
 
-    // Verifica se a posição é inválida
+    
     if (n <= 0 || L == NULL) {
         printf("Erro fatal: elemento invalido.\n");
         exit(1);
     }
 
-    // Caso base
+    
     if (n == 1) {
         return L->valor;
     }
 
-    // Chamada recursiva
+    
     return enesimo(n - 1, L->prox);
 }
 
 
 int main() {
 
-    // Criando os nós
+    
     No d = {'d', NULL};
     No c = {'c', &d};
     No b = {'b', &c};
